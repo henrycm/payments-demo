4fe21e2861f487f5027c7b26061dcf1e5ea9e0d4
Henry <henrycm@gmail.com>
2023-01-18 00:04:06 -0500

    Add kafka config

--- src/main/java/com/payments/demo/Application.java
+++ src/main/java/com/payments/demo/Application.java
@@ -1,7 +1,17 @@
 package com.payments.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +20,29 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Value("${kafka.broker.url}")
+	private String kafkaBrokerUrl;
+
+	@Bean
+	public ProducerFactory<String, String> producerFactory() {
+		return new DefaultKafkaProducerFactory<>(producerConfigs());
+	}
+
+	@Bean
+	public Map<String, Object> producerConfigs() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokerUrl);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		//props.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, "10");
+		props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "5");
+		props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "5");
+		props.put(ProducerConfig.RETRIES_CONFIG, "0");
+		return props;
+	}
+
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		return new KafkaTemplate<String, String>(producerFactory());
+	}
 }
--- src/main/java/com/payments/demo/PaymentService.java
+++ src/main/java/com/payments/demo/PaymentService.java
@@ -2,8 +2,11 @@ package com.payments.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.payments.demo.model.Payment;
 import com.payments.demo.model.PaymentStatus;
 
@@ -16,11 +19,23 @@ public class PaymentService {
 
     @Resource
     private PaymentRepository repo;
+    @Resource
+    ObjectMapper objectMapper;
+    @Resource
+    private KafkaTemplate<String, String> kafkaTemplate;
+    @Value("${kafka.topic.name}")
+    String kafkaTopicName;
 
     @Transactional
     void create(Payment payment) {
-        LOGGER.info("Creating payment " + payment);
+        LOGGER.info("Creating payment: " + payment);
         payment.setStatus(PaymentStatus.CREATED);
         repo.save(payment);
+
+        try {
+            kafkaTemplate.send(kafkaTopicName, objectMapper.writeValueAsString(payment)).get();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
